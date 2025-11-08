3f550c23b7613a179a9c35fde2d576124fdff2fa
Joemekka <mekka988@gmail.com>
2025-11-08 17:45:15 +0100

    number2 variable fiexd

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,5 +1,5 @@
 number1 = 10
-nummber2 = 5
+number2 = 5
 
 print('Addition of', number1, 'And', nummber2, 'is', number1 + nummber2  )
 print('Subtraction of', number1, 'And', nummber2, 'is', number1 - nummber2  )
