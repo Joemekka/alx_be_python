581b8f22878ebeada17f9011f4027d93c8d7f21e
Joemekka <mekka988@gmail.com>
2025-11-08 17:59:08 +0100

    monthly savings fiexed

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
 monthly_income = float(input('Enter your monthly income: '))
 monthly_expenses = float(input('Enter your monthly expenses: '))
 
-monthlySavings = monthly_income - monthly_expenses
-projectedSavings = monthlySavings * 12 + (monthlySavings * 12 * 0.05)
-print(f'Your monthly savings are: ${monthlySavings}')
+monthly_savings = monthly_income - monthly_expenses
+projectedSavings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+print(f'Your monthly savings are: ${monthly_savings}')
 print(f'Projected savings after one year with interest is: ${projectedSavings}')
