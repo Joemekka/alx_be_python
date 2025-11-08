bdaadc0f5abfe39f1edf6d05fcef69949c91a1f6
Joemekka <mekka988@gmail.com>
2025-11-08 17:46:35 +0100

    number spelling fiexed in string

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,6 @@
 number1 = 10
 number2 = 5
 
-print('Addition of', number1, 'And', nummber2, 'is', number1 + nummber2  )
-print('Subtraction of', number1, 'And', nummber2, 'is', number1 - nummber2  )
-print('Multiplication of', number1, 'And', nummber2, 'is', number1 * nummber2  )
+print('Addition of', number1, 'And', number2, 'is', number1 + number2  )
+print('Subtraction of', number1, 'And', number2, 'is', number1 - number2  )
+print('Multiplication of', number1, 'And', number2, 'is', number1 * number2  )
