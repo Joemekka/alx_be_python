4bf506e6d2cff6f6ad4ecf69613f7504ef718d72
Joemekka <mekka988@gmail.com>
2025-11-08 17:42:31 +0100

    first

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,6 @@
+number1 = 10
+nummber2 = 5
+
+print('Addition of', number1, 'And', nummber2, 'is', number1 + nummber2  )
+print('Subtraction of', number1, 'And', nummber2, 'is', number1 - nummber2  )
+print('Multiplication of', number1, 'And', nummber2, 'is', number1 * nummber2  )
