547b435d8c62011a2dcaff4f329a5f7954009727
Joemekka <mekka988@gmail.com>
2025-11-17 00:50:55 +0100

    major fixes

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
 number = int(input('Enter a number to see its multiplication table:'))
 
-for n in range(1, number + 1):
+for s in range(1, 11):
         print(f'{number} * {n} =', number * n)
--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,10 @@
-inputUser = input("What's the weather like today? (sunny/rainy/cold):")
+weatherg = input("What's the weather like today? (sunny/rainy/cold):")
 
-if inputUser == 'sunny':
+if weatherg == 'sunny':
         print('Wear a t-shirt and sunglasses.')
-elif inputUser == 'rainy':
+elif weatherg == 'rainy':
         print("Don't forget your umbrella and a raincoat.")
-elif inputUser == 'cold':
+elif weatherg == 'cold':
         print("Make sure to wear a warm coat and a scarf.")
 else:
         print("Sorry, I don't have recommendations for this weather.")
