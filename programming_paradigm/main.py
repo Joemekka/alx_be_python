1627ce945b65b133cda5a211ff43979c15d67642
Joemekka <mekka988@gmail.com>
2025-12-14 12:32:01 +0100

    rere

--- programming_paradigm/main.py
+++ programming_paradigm/main.py
@@ -9,8 +9,8 @@ def main():
         print("Usage: python main.py <numerator> <denominator>")
         sys.exit(1)
 
-    numerator = sys.argv[1]
-    denominator = sys.argv[2]
+    numerator = float(sys.argv[1])
+    denominator = float(sys.argv[2])
 
     safe_divide(numerator, denominator)
 
